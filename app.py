6e72935d0c633f4dac1789a7db6091d05fe78cbe
Anusha <anushachand04@gmail.com>
2020-06-05 11:21:32 -0700

    commiting portfolio

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask_portfolio import application 
+
+# This is so we can run the application through python
+if __name__ == '__main__':
+    #application.run(host='0.0.0.0')
+    application.run(debug=True)
